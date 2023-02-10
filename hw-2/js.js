7d0fc341ec0997d4339aa5738f7840cbf4447d39
Tymur Halimov <galtimur1966@gmail.com>
2023-02-10 17:56:08 +0200

    Added HW 2

--- hw-2/js.js
+++ hw-2/js.js
@@ -1,5 +1,4 @@
-do {
-    num1 = +prompt("Введите первое число", "2"), 
+let num1 = +prompt("Введите первое число", "2"), 
     num2 = +prompt("Введите второе число", "2"); 
-    } while ( isNaN(num1) || isNaN(num2) )
+    ( isNaN(num1) || isNaN(num2) )
     alert(num1 + num2);
