2dcfec745e473182b8c9150b75fb3a3449b208e5
Tymur Halimov <galtimur1966@gmail.com>
2023-02-11 14:48:34 +0200

    delete ()

--- hw-4/js.js
+++ hw-4/js.js
@@ -1,4 +1,4 @@
-let number1 = +prompt('Input number 1', ''),
-    number2 = +prompt('Input number 2', ''),
-    number3 = +prompt('Input number 3', '');
+let number1 = +prompt('Input number 1'),
+    number2 = +prompt('Input number 2'),
+    number3 = +prompt('Input number 3');
     alert((number1 + number2 + number3)/3);
