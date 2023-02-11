d3ed18e93e3c6df760c8f1cef4c8fa01fc5f7890
Tymur Halimov <galtimur1966@gmail.com>
2023-02-11 15:07:05 +0200

    sdfs

--- hw-2/js.js
+++ hw-2/js.js
@@ -4,4 +4,5 @@ let num1 = +prompt("Введите первое число", "2"),
     alert(num1 + num2);
     alert(num1 - num2);
     alert(num1 * num2);
-    alert(num1 / num2);   
+    alert(num1 / num2);
+       
