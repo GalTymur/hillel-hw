1b98ec89a351d13d26d3ef917b541661ea99ddfb
Tymur Halimov <galtimur1966@gmail.com>
2023-02-11 14:52:51 +0200

    add formula

--- hw-2/js.js
+++ hw-2/js.js
@@ -2,3 +2,6 @@ let num1 = +prompt("Введите первое число", "2"),
     num2 = +prompt("Введите второе число", "2"); 
     ( isNaN(num1) || isNaN(num2) )
     alert(num1 + num2);
+    alert(num1 - num2);
+    alert(num1 * num2);
+    alert(num1 / num2);
